4a72026d66b3533cb9a07aa4d8bccc82d03cc986
Victor Wei <victoryqwei@gmail.com>
2023-08-25 01:16:54 -0400

    feat: very basic website

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import vectorLogo from "./assets/vector.png";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://victorwei.com" target="_blank">
+          <img src={vectorLogo} className="logo" alt="Vector logo" />
         </a>
       </div>
       <h1>Hey, I'm Victor.</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR yes
-        </p>
+        <p>Website is currently under construction. Check back later for updates!</p>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
 }
