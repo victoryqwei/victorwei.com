dd6709b833969ae17337bc099777a07ff02d483c
Victor Wei <victoryqwei@gmail.com>
2023-08-25 18:43:32 -0400

    feat: convert to chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,52 @@
+import { Box, Center, Heading, Image, Link, Text, VStack } from "@chakra-ui/react";
 import vectorLogo from "./assets/vector.png";
-import "./App.css";
+import "./index.css";
 
-function App() {
+const App: React.FC = () => {
   return (
     <>
-      <div>
-        <a href="https://victorwei.com" target="_blank">
-          <img src={vectorLogo} className="logo" alt="Vector logo" />
-        </a>
-      </div>
-      <h1>Hey, I'm Victor.</h1>
-      <div className="card">
-        <p>Website is currently under construction. Check back later for updates!</p>
-      </div>
-      <p className="old-website">
-        Or check out my old personal website <a href="https://old.victorwei.com">here</a>
-      </p>
+      <Center height="100vh" padding="2rem" textAlign="center" color="rgba(255, 255, 255, 0.87)" lineHeight="1.5">
+        <VStack>
+          <Box padding="1.5em">
+            <Link href="https://victorwei.com" target="_blank">
+              <Image
+                src={vectorLogo}
+                alt="Vector logo"
+                height="6em"
+                transition="filter 300ms"
+                _hover={{
+                  filter: "drop-shadow(0 0 2em red)",
+                }}
+              />
+            </Link>
+          </Box>
+
+          <Heading fontSize="3.2em" lineHeight="1.1">
+            Hey, I'm Victor.
+          </Heading>
+
+          <Box padding="2em">
+            <Text>Website is currently under construction. Check back later for updates!</Text>
+          </Box>
+
+          <Box padding="2em">
+            <Text color="#888">
+              Or check out my old personal website{" "}
+              <Link
+                color="#646cff"
+                fontWeight="500"
+                href="https://old.victorwei.com"
+                _hover={{
+                  color: "#535bf2",
+                }}>
+                here
+              </Link>
+            </Text>
+          </Box>
+        </VStack>
+      </Center>
     </>
   );
-}
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { theme } from "./utils/theme.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
