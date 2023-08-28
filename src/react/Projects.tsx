091d352a528ce7dcfc2231c873871856e8c6112a
Victor Wei <victoryqwei@gmail.com>
2023-08-27 23:25:33 -0400

    wip: featured projects

--- src/react/Projects.tsx
+++ src/react/Projects.tsx
@@ -1,4 +1,5 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, HStack, Heading, Image, Link, Text, VStack } from "@chakra-ui/react";
+import miniblox from "../assets/miniblox.png";
 
 const Projects: React.FC = () => {
   return (
@@ -6,6 +7,36 @@ const Projects: React.FC = () => {
       <Heading fontSize="2xl" mb="1em">
         Featured Projects
       </Heading>
+
+      <HStack h="25em">
+        <Link href="https://miniblox.io" target="_blank">
+          <Image
+            src={miniblox}
+            filter="blur(1.5px)"
+            transition="filter 0.3s"
+            _hover={{
+              filter: "none",
+            }}
+            borderRadius="2px"></Image>
+        </Link>
+
+        <VStack zIndex={1} gap="1em" align="flex-end" p="1em">
+          <Heading fontSize="3xl">Miniblox</Heading>
+          <Box p="1em" bgColor="grey" w="120%">
+            <Text>
+              Miniblox is an online multiplayer voxel game that offers a variety of features, including competitive minigames, a wide
+              selection of blocks to build with, challenging parkour levels, and much more!
+            </Text>
+          </Box>
+          <HStack>
+            <Text>Typescript</Text>
+            <Text>Three.js</Text>
+            <Text>Grafana</Text>
+            <Text>Kubernetes</Text>
+            <Text>Kubernetes</Text>
+          </HStack>
+        </VStack>
+      </HStack>
     </Box>
   );
 };
