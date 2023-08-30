630c54fce855ed1f7b4efeb99cad2af3de7c8c32
Victor Wei <victoryqwei@gmail.com>
2023-08-29 22:14:01 -0400

    fix: update contact margins

--- src/react/Contact.tsx
+++ src/react/Contact.tsx
@@ -6,46 +6,48 @@ import SocialIcon from "./components/SocialIcon";
 const Contact: React.FC = () => {
   const [isMobile] = useMobile();
   return (
-    <Center id="contact" py="2em" px="2em" h="100vh" w="100%" textAlign="center">
-      <VStack gap={0} justifyContent="space-between" h="100%">
-        <Box></Box>
+    <>
+      <Center id="contact" py="10em" px="2em" w="100%" textAlign="center">
+        <VStack gap={0} justifyContent="space-between" h="100%">
+          <Box></Box>
 
-        <VStack gap="1em">
-          <Heading fontSize="4xl">Get In Touch</Heading>
-          <Text mb="3em">If you have any questions or just want to say hi, feel free to reach out to me!</Text>
-          <Link href="mailto:victoryqwei@gmail.com" target="_blank">
-            <Button
-              backgroundColor="transparent"
-              border="
+          <VStack gap="1em">
+            <Heading fontSize="4xl">Get In Touch</Heading>
+            <Text mb="3em">If you have any questions or just want to say hi, feel free to reach out to me!</Text>
+            <Link href="mailto:victoryqwei@gmail.com" target="_blank">
+              <Button
+                backgroundColor="transparent"
+                border="
                 1px solid white"
-              color="white"
-              p="1em"
-              _hover={{
-                backgroundColor: "rgba(255, 255, 255, 0.1)",
-              }}>
-              Contact Me!
-            </Button>
-          </Link>
+                color="white"
+                p="1em"
+                _hover={{
+                  backgroundColor: "rgba(255, 255, 255, 0.1)",
+                }}>
+                Contact Me!
+              </Button>
+            </Link>
+          </VStack>
         </VStack>
-        <VStack gap="1em">
-          {isMobile && (
-            <HStack gap="1.5em">
-              <SocialIcon icon={<FiGithub size="1.3em" />} link="https://github.com/victoryqwei" />
-              <SocialIcon icon={<FiLinkedin size="1.3em" />} link="https://www.linkedin.com/in/victoryqwei/" />
-              <SocialIcon icon={<FiInstagram size="1.3em" />} link="https://www.instagram.com/victoryqwei/" />
-              <SocialIcon icon={<FiMail size="1.3em" />} link="mailto:victoryqwei@gmail.com" />
-            </HStack>
-          )}
-          <Text>
-            website design inspired by{" "}
-            <Link href="https://v4.brittanychiang.com/" isExternal>
-              brittany
-            </Link>{" "}
-            | © 2023 Victor Wei
-          </Text>
-        </VStack>
+      </Center>
+      <VStack gap="1em" py="2em" px="2em">
+        {isMobile && (
+          <HStack gap="1.5em">
+            <SocialIcon icon={<FiGithub size="1.3em" />} link="https://github.com/victoryqwei" />
+            <SocialIcon icon={<FiLinkedin size="1.3em" />} link="https://www.linkedin.com/in/victoryqwei/" />
+            <SocialIcon icon={<FiInstagram size="1.3em" />} link="https://www.instagram.com/victoryqwei/" />
+            <SocialIcon icon={<FiMail size="1.3em" />} link="mailto:victoryqwei@gmail.com" />
+          </HStack>
+        )}
+        <Text>
+          website design inspired by{" "}
+          <Link href="https://v4.brittanychiang.com/" isExternal>
+            brittany
+          </Link>{" "}
+          | © 2023 Victor Wei
+        </Text>
       </VStack>
-    </Center>
+    </>
   );
 };
 
