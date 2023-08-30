a3744a4e21c7ab25f49919cc0e980e6ddc226405
Victor Wei <victoryqwei@gmail.com>
2023-08-30 18:50:09 -0400

    wip: add rope cursor

---
+++ src/gui/Vector.ts
@@ -0,0 +1,108 @@
+class Vector {
+  x: number;
+  y: number;
+  constructor(x: number, y: number) {
+    this.x = x || 0;
+    this.y = y || 0;
+  }
+  set(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+  getMag() {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
+  setMag(magnitude: number) {
+    const direction = this.getDir();
+    this.x = Math.cos(direction) * magnitude;
+    this.y = Math.sin(direction) * magnitude;
+  }
+  getDir() {
+    return Math.atan2(this.y, this.x);
+  }
+  add(a: Vector, b?: Vector) {
+    if (b) {
+      return new Vector(a.x + b.x, a.y + b.y);
+    } else {
+      this.x = this.x + a.x;
+      this.y = this.y + a.y;
+    }
+  }
+  static add(a: Vector, b: Vector) {
+    return new Vector(a.x + b.x, a.y + b.y);
+  }
+  sub(a: Vector, b?: Vector) {
+    if (b) {
+      return new Vector(a.x - b.x, a.y - b.y);
+    } else {
+      this.x = this.x - a.x;
+      this.y = this.y - a.y;
+    }
+  }
+  static sub(a: Vector, b: Vector) {
+    return new Vector(a.x - b.x, a.y - b.y);
+  }
+  mult(scalar: number) {
+    this.x = this.x * scalar;
+    this.y = this.y * scalar;
+  }
+  static mult(vector: Vector, scalar: number) {
+    return new Vector(vector.x * scalar, vector.y * scalar);
+  }
+  div(scalar: number) {
+    this.x = this.x / scalar;
+    this.y = this.y / scalar;
+  }
+  static div(vector: Vector, scalar: number) {
+    return new Vector(vector.x / scalar, vector.y / scalar);
+  }
+  getDot(b: Vector) {
+    return this.x * b.x + this.y * b.y;
+  }
+  static dot(a: Vector, b: Vector) {
+    return a.x * b.x + a.y * b.y;
+  }
+  static cross(a: Vector, b: Vector) {
+    return a.x * b.y - a.y * b.x;
+  }
+  static dist(a: Vector, b: Vector) {
+    return Math.sqrt(Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2));
+  }
+  normalize() {
+    if (this.getMag() != 0) {
+      this.div(this.getMag());
+    }
+  }
+  limit(max: number) {
+    if (this.getMag() > max) {
+      this.normalize();
+      this.mult(max);
+    }
+  }
+  copy() {
+    return new Vector(this.x, this.y);
+  }
+  rotate(ang: number) {
+    ang = ang * (Math.PI / 180);
+    const cos = Math.cos(ang);
+    const sin = Math.sin(ang);
+    this.x = Math.round(10000 * (this.x * cos - this.y * sin)) / 10000;
+    this.y = Math.round(10000 * (this.x * sin + this.y * cos)) / 10000;
+  }
+  static rotate(
+    vector: Vector,
+    ang: number // Vector to rotate by an angle in radians
+  ) {
+    const cos = Math.cos(ang);
+    const sin = Math.sin(ang);
+    return new Vector(
+      Math.round(10000 * (vector.x * cos - vector.y * sin)) / 10000,
+      Math.round(10000 * (vector.x * sin + vector.y * cos)) / 10000
+    );
+  }
+  print() {
+    console.log("X: " + this.x + " Y: " + this.y);
+  }
+}
+
+export default Vector;
--- src/react/CursorCanvas.tsx
+++ src/react/CursorCanvas.tsx
@@ -1,10 +1,128 @@
 import { useEffect, useRef } from "react";
+import Vector from "../gui/vector";
 
 // cursor canvas
 const mouse = {
   x: 0,
   y: 0,
 };
+
+const gravity = new Vector(0, 9.81);
+const dt = 4 / 1000;
+
+class Node {
+  prevPos: Vector;
+  constructor(readonly pos: Vector) {
+    this.pos = new Vector(pos.x, pos.y);
+    this.prevPos = this.pos.copy();
+  }
+
+  update() {
+    const old = this.pos.copy();
+
+    this.pos.add(Vector.add(Vector.sub(this.pos, this.prevPos), Vector.mult(gravity, dt * dt * 100)));
+
+    this.prevPos = old;
+  }
+
+  draw(ctx: CanvasRenderingContext2D) {
+    ctx.beginPath();
+    ctx.arc(this.pos.x, this.pos.y, 5, 0, 2 * Math.PI, false);
+    ctx.fillStyle = "white";
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
+class Rope {
+  width: number = 10;
+  iterations: number = 80;
+  totalNodes: number = 80;
+  nodeDistance: number = 10;
+  nodes: Node[] = [];
+  locked: boolean = true;
+
+  constructor() {
+    const { totalNodes, nodes, nodeDistance } = this;
+
+    const pos = new Vector(window.innerHeight / 2, window.innerHeight / 2);
+    for (let i = 0; i < totalNodes; i++) {
+      nodes[i] = new Node(pos);
+      pos.y -= nodeDistance;
+      pos.x += Math.random();
+    }
+  }
+
+  update() {
+    this.simulate();
+    for (let i = 0; i < this.iterations; i++) {
+      this.applyConstraints();
+    }
+  }
+
+  simulate() {
+    for (const node of this.nodes) {
+      node.update();
+    }
+  }
+
+  applyConstraints() {
+    for (let i = 0; i < this.nodes.length - 1; i++) {
+      const node1 = this.nodes[i];
+      const node2 = this.nodes[i + 1];
+
+      if (i == 0) {
+        node1.pos.set(mouse.x, mouse.y);
+      }
+
+      if (node1.pos.y > window.innerHeight - this.width / 2) {
+        node1.pos.y = window.innerHeight - this.width / 2;
+        node1.prevPos.x += (node1.pos.x - node1.prevPos.x) / 2000;
+      }
+      if (node1.pos.y < 0) {
+        node1.pos.y = 0;
+      }
+      if (node1.pos.x > window.innerWidth) {
+        node1.pos.x = window.innerWidth;
+      }
+      if (node1.pos.x < 0) {
+        node1.pos.x = 0;
+      }
+
+      const diff = Vector.sub(node1.pos, node2.pos);
+      const dist = Vector.dist(node1.pos, node2.pos);
+      let difference = 0;
+      if (dist > 0) {
+        difference = (this.nodeDistance - dist) / dist;
+      }
+
+      diff.mult(0.5 * difference);
+
+      const translate = diff.copy();
+
+      node1.pos.add(translate);
+      node2.pos.sub(translate);
+    }
+  }
+
+  draw(ctx: CanvasRenderingContext2D) {
+    for (let i = 0; i < this.nodes.length - 1; i++) {
+      const node1 = this.nodes[i];
+      const node2 = this.nodes[i + 1];
+
+      ctx.beginPath();
+      ctx.lineWidth = this.width;
+      ctx.strokeStyle = "white";
+      ctx.lineCap = "round";
+      ctx.moveTo(node1.pos.x, node1.pos.y);
+      ctx.lineTo(node2.pos.x, node2.pos.y);
+      ctx.stroke();
+      ctx.closePath();
+    }
+  }
+}
+
+const rope = new Rope();
 
 function loop(canvas: HTMLCanvasElement) {
   const ctx = canvas.getContext("2d")!;
@@ -14,8 +132,11 @@ function loop(canvas: HTMLCanvasElement) {
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  ctx.fillStyle = "rgba(255, 255, 255, 1)";
-  ctx.fillRect(mouse.x, mouse.y, 100, 100);
+  rope.update();
+  rope.draw(ctx);
+
+  // ctx.fillStyle = "rgba(255, 255, 255, 1)";
+  // ctx.fillRect(mouse.x, mouse.y, 100, 100);
 }
 
 const CursorCanvas: React.FC = () => {
